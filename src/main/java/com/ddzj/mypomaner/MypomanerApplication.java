1eb949c4d3c11dfedfafd754d0c914ca424fb660
yangfengchen <377681869@qq.com>
2023-12-18 00:33:32 +0800

    Initial commit

---
+++ src/main/java/com/ddzj/mypomaner/MypomanerApplication.java
@@ -0,0 +1,13 @@
+package com.ddzj.mypomaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypomanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypomanerApplication.class, args);
+	}
+
+}
